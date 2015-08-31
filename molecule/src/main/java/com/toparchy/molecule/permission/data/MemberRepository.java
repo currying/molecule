0c86ffb8386658fcc24662b2dcd4625f96c337e2
王宇轩 <currying@gmail.com>
2015-08-31 17:25:19 +0800

    Signed-off-by: 王宇轩 <currying@gmail.com>

--- molecule/src/main/java/com/toparchy/molecule/permission/data/MemberRepository.java
+++ molecule/src/main/java/com/toparchy/molecule/permission/data/MemberRepository.java
@@ -1,14 +1,19 @@
 package com.toparchy.molecule.permission.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.RelationshipManager;
 import org.picketlink.idm.model.IdentityType;
+import org.picketlink.idm.model.basic.Group;
+import org.picketlink.idm.model.basic.GroupMembership;
 import org.picketlink.idm.query.IdentityQuery;
 import org.picketlink.idm.query.IdentityQueryBuilder;
+import org.picketlink.idm.query.RelationshipQuery;
 
 import com.toparchy.molecule.permission.model.Member;
 
@@ -17,6 +22,9 @@ public class MemberRepository {
 
 	@Inject
 	private IdentityManager identityManager;
+
+	@Inject
+	private RelationshipManager relationshipManager;
 
 	public Member findById(String id) {
 		IdentityQueryBuilder queryBuilder = identityManager.getQueryBuilder();
@@ -29,5 +37,20 @@ public class MemberRepository {
 		IdentityQueryBuilder queryBuilder = identityManager.getQueryBuilder();
 		IdentityQuery<Member> query = queryBuilder.createIdentityQuery(Member.class);
 		return query.getResultList();
+	}
+
+	public List<Group> findMemberGroup(Member member) {
+		RelationshipQuery<GroupMembership> query = this.relationshipManager
+				.createRelationshipQuery(GroupMembership.class);
+
+		query.setParameter(GroupMembership.MEMBER, member);
+
+		List<Group> groups = new ArrayList<Group>();
+
+		for (GroupMembership groupMembership : query.getResultList()) {
+			groups.add((Group) groupMembership.getGroup());
+		}
+
+		return groups;
 	}
 }

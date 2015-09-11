08fb0b58165481c6cd477e0dab0f558b8ea5bb2e
王宇轩 <currying@gmail.com>
2015-09-11 15:39:53 +0800

    增加用户角色维护
    Signed-off-by: 王宇轩 <currying@gmail.com>

--- molecule/src/main/java/com/toparchy/molecule/permission/HttpSecurityConfiguration.java
+++ molecule/src/main/java/com/toparchy/molecule/permission/HttpSecurityConfiguration.java
@@ -1,21 +1,36 @@
 package com.toparchy.molecule.permission;
 
+import javax.enterprise.event.Observes;
+
 import org.picketlink.config.SecurityConfigurationBuilder;
 import org.picketlink.event.SecurityConfigurationEvent;
-
-import javax.enterprise.event.Observes;
 
 public class HttpSecurityConfiguration {
 
 	public void onInit(@Observes SecurityConfigurationEvent event) {
 		SecurityConfigurationBuilder builder = event.getBuilder();
 
-		builder.http().forGroup("basic").authenticateWith().form()
-				.authenticationUri("/login.jsf").loginPage("/login.jsf")
-				.errorPage("/error.jsf").restoreOriginalRequest()
-				.forPath("/logout").logout().redirectTo("/index.html")
-				.forPath("/view/*", "basic").forPath("/run", "basic")
-				.forPath("/frameset", "basic").forPath("/webcontent", "basic")
-				.forPath("/report", "basic").forPath("/rest/business/*", "basic");
+		builder
+			.http()
+				.forGroup("basic")
+					.authenticateWith()
+						.form()
+							.authenticationUri("/login.jsf")
+								.loginPage("/login.jsf")
+								.errorPage("/error.jsf")
+								.restoreOriginalRequest()
+				.forPath("/logout").logout()
+					.redirectTo("/index.html")
+				.forPath("/view/*", "basic")
+				.forPath("/run", "basic")
+				.forPath("/frameset", "basic")
+				.forPath("/webcontent", "basic")
+				.forPath("/report", "basic")
+				.forPath("/rest/business/*", "basic")
+				.forPath("/system/*", "basic")
+//					.authorizeWith()
+//					.group("Administrators")
+					.authorizeWith()
+					.role("ADMINISTRATOR");
 	}
 }

e5fedfc9e1d477dc503e71621ac359ce0b3a47e3
currying <currying@CURRYING-PC>
2015-10-08 08:47:03 +0800

    Signed-off-by: currying <currying@CURRYING-PC>

--- molecule/src/main/java/org/primefaces/sentinel/component/menu/SentinelMenu.java
+++ molecule/src/main/java/org/primefaces/sentinel/component/menu/SentinelMenu.java
@@ -21,12 +21,10 @@ import javax.faces.component.UINamingContainer;
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
 
-@ResourceDependencies({
-	@ResourceDependency(library="primefaces", name="primefaces.css"),
-	@ResourceDependency(library="primefaces", name="jquery/jquery.js"),
-	@ResourceDependency(library="primefaces", name="jquery/jquery-plugins.js"),
-	@ResourceDependency(library="primefaces", name="primefaces.js")
-})
+@ResourceDependencies({ @ResourceDependency(library = "primefaces", name = "primefaces.css"),
+		@ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
+		@ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js"),
+		@ResourceDependency(library = "primefaces", name = "primefaces.js") })
 public class SentinelMenu extends AbstractMenu implements org.primefaces.component.api.Widget {
 
 	public static final String COMPONENT_TYPE = "org.primefaces.component.SentinelMenu";
@@ -34,10 +32,7 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 	private static final String DEFAULT_RENDERER = "org.primefaces.component.SentinelMenuRenderer";
 
 	protected enum PropertyKeys {
-		widgetVar
-		,model
-		,style
-		,styleClass;
+		widgetVar, model, style, styleClass;
 
 		String toString;
 
@@ -45,11 +40,12 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 			this.toString = toString;
 		}
 
-		PropertyKeys() {}
+		PropertyKeys() {
+		}
 
 		public String toString() {
 			return ((this.toString != null) ? this.toString : super.toString());
-}
+		}
 	}
 
 	public SentinelMenu() {
@@ -63,6 +59,7 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 	public java.lang.String getWidgetVar() {
 		return (java.lang.String) getStateHelper().eval(PropertyKeys.widgetVar, null);
 	}
+
 	public void setWidgetVar(java.lang.String _widgetVar) {
 		getStateHelper().put(PropertyKeys.widgetVar, _widgetVar);
 	}
@@ -70,6 +67,7 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 	public org.primefaces.model.menu.MenuModel getModel() {
 		return (org.primefaces.model.menu.MenuModel) getStateHelper().eval(PropertyKeys.model, null);
 	}
+
 	public void setModel(org.primefaces.model.menu.MenuModel _model) {
 		getStateHelper().put(PropertyKeys.model, _model);
 	}
@@ -77,6 +75,7 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 	public java.lang.String getStyle() {
 		return (java.lang.String) getStateHelper().eval(PropertyKeys.style, null);
 	}
+
 	public void setStyle(java.lang.String _style) {
 		getStateHelper().put(PropertyKeys.style, _style);
 	}
@@ -84,6 +83,7 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 	public java.lang.String getStyleClass() {
 		return (java.lang.String) getStateHelper().eval(PropertyKeys.styleClass, null);
 	}
+
 	public void setStyleClass(java.lang.String _styleClass) {
 		getStateHelper().put(PropertyKeys.styleClass, _styleClass);
 	}
@@ -92,9 +92,9 @@ public class SentinelMenu extends AbstractMenu implements org.primefaces.compone
 		FacesContext context = getFacesContext();
 		String userWidgetVar = (String) getAttributes().get("widgetVar");
 
-		if(userWidgetVar != null)
+		if (userWidgetVar != null)
 			return userWidgetVar;
-		 else
+		else
 			return "widget_" + getClientId(context).replaceAll("-|" + UINamingContainer.getSeparatorChar(context), "_");
 	}
 }

c9775eb1dd82fdca3e44d7071fdd2dd7bb84410c
currying <currying@CURRYING-PC>
2015-10-09 16:47:48 +0800

    Signed-off-by: currying <currying@CURRYING-PC>

--- molecule/src/main/java/com/toparchy/molecule/netty/NettyListener.java
+++ molecule/src/main/java/com/toparchy/molecule/netty/NettyListener.java
@@ -4,7 +4,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-@WebListener
+//@WebListener
 public class NettyListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent sce) {

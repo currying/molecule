6b819154922c143ab1dddbddc06011c3fb454bee
currying <currying@CURRYING-PC>
2015-10-14 15:29:27 +0800

    Signed-off-by: currying <currying@CURRYING-PC>

--- molecule/src/test/java/com/toparchy/serial/EchoClientHandler.java
+++ molecule/src/test/java/com/toparchy/serial/EchoClientHandler.java
@@ -1,54 +1,54 @@
-package com.toparchy.serial;
-
-import io.netty.channel.ChannelHandlerAdapter;
-import io.netty.channel.ChannelHandlerContext;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class EchoClientHandler extends ChannelHandlerAdapter {
-	private final int sendNumber;
-
-	public EchoClientHandler(int sendNumber) {
-		this.sendNumber = sendNumber;
-	}
-
-	@Override
-	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		List<UserInfo> userInfos = UserInfo();
-		for (UserInfo info : userInfos) {
-			ctx.write(info);
-		}
-		ctx.flush();
-	}
-
-	private List<UserInfo> UserInfo() {
-		List<UserInfo> userInfos = new ArrayList<UserInfo>();
-		UserInfo userInfo = null;
-		for (int i = 0; i < sendNumber; i++) {
-			userInfo = new UserInfo();
-			userInfo.setName("ABCDEFG --->" + i);
-			userInfo.setAge(i);
-			userInfos.add(userInfo);
-		}
-		return userInfos;
-	}
-
-	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg)
-			throws Exception {
-		System.out.println("Client receive the msgpack message : " + msg);
-	}
-
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-			throws Exception {
-		cause.printStackTrace();
-		ctx.close();
-	}
-
-	@Override
-	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		ctx.flush();
-	}
-}
+//package com.toparchy.serial;
+//
+//import io.netty.channel.ChannelHandlerAdapter;
+//import io.netty.channel.ChannelHandlerContext;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class EchoClientHandler extends ChannelHandlerAdapter {
+//	private final int sendNumber;
+//
+//	public EchoClientHandler(int sendNumber) {
+//		this.sendNumber = sendNumber;
+//	}
+//
+//	@Override
+//	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//		List<UserInfo> userInfos = UserInfo();
+//		for (UserInfo info : userInfos) {
+//			ctx.write(info);
+//		}
+//		ctx.flush();
+//	}
+//
+//	private List<UserInfo> UserInfo() {
+//		List<UserInfo> userInfos = new ArrayList<UserInfo>();
+//		UserInfo userInfo = null;
+//		for (int i = 0; i < sendNumber; i++) {
+//			userInfo = new UserInfo();
+//			userInfo.setName("ABCDEFG --->" + i);
+//			userInfo.setAge(i);
+//			userInfos.add(userInfo);
+//		}
+//		return userInfos;
+//	}
+//
+//	@Override
+//	public void channelRead(ChannelHandlerContext ctx, Object msg)
+//			throws Exception {
+//		System.out.println("Client receive the msgpack message : " + msg);
+//	}
+//
+//	@Override
+//	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+//			throws Exception {
+//		cause.printStackTrace();
+//		ctx.close();
+//	}
+//
+//	@Override
+//	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+//		ctx.flush();
+//	}
+//}
--- molecule/src/test/java/com/toparchy/serial/EchoServer.java
+++ molecule/src/test/java/com/toparchy/serial/EchoServer.java
@@ -1,75 +1,75 @@
-package com.toparchy.serial;
-
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
-import io.netty.handler.codec.serialization.ClassResolvers;
-import io.netty.handler.codec.serialization.ObjectDecoder;
-import io.netty.handler.codec.serialization.ObjectEncoder;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
-
-public class EchoServer {
-
-	private final int port;
-
-	public EchoServer(int port) {
-		this.port = port;
-	}
-
-	public void run() {
-		EventLoopGroup boss = new NioEventLoopGroup();
-		EventLoopGroup worker = new NioEventLoopGroup();
-		try {
-			ServerBootstrap b = new ServerBootstrap();
-			b.group(boss, worker).channel(NioServerSocketChannel.class)
-					.option(ChannelOption.SO_BACKLOG, 100)
-					.childOption(ChannelOption.SO_KEEPALIVE, true)
-					.handler(new LoggingHandler(LogLevel.INFO))
-					.childHandler(new ChannelInitializer<SocketChannel>() {
-						@Override
-						protected void initChannel(SocketChannel ch)
-								throws Exception {
-							ch.pipeline().addLast(
-									"frameDecoder",
-									new LengthFieldBasedFrameDecoder(65535, 0,
-											2, 0, 2));
-							ch.pipeline()
-									.addLast(
-											"msgpack decoder",
-											new ObjectDecoder(
-													1024 * 1024,
-													ClassResolvers
-															.weakCachingConcurrentResolver(this
-																	.getClass()
-																	.getClassLoader())));
-							ch.pipeline().addLast("frameEncoder",
-									new LengthFieldPrepender(2));
-							ch.pipeline().addLast("msgpack encoder",
-									new ObjectEncoder());
-							ch.pipeline().addLast(new EchoServerHandler());
-						}
-					});
-
-			ChannelFuture future = b.bind(port).sync();
-			future.channel().closeFuture().sync();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			boss.shutdownGracefully();
-			worker.shutdownGracefully();
-		}
-	}
-
-	public static void main(String[] args) {
-		new EchoServer(8000).run();
-	}
-
-}
+//package com.toparchy.serial;
+//
+//import io.netty.bootstrap.ServerBootstrap;
+//import io.netty.channel.ChannelFuture;
+//import io.netty.channel.ChannelInitializer;
+//import io.netty.channel.ChannelOption;
+//import io.netty.channel.EventLoopGroup;
+//import io.netty.channel.nio.NioEventLoopGroup;
+//import io.netty.channel.socket.SocketChannel;
+//import io.netty.channel.socket.nio.NioServerSocketChannel;
+//import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+//import io.netty.handler.codec.LengthFieldPrepender;
+//import io.netty.handler.codec.serialization.ClassResolvers;
+//import io.netty.handler.codec.serialization.ObjectDecoder;
+//import io.netty.handler.codec.serialization.ObjectEncoder;
+//import io.netty.handler.logging.LogLevel;
+//import io.netty.handler.logging.LoggingHandler;
+//
+//public class EchoServer {
+//
+//	private final int port;
+//
+//	public EchoServer(int port) {
+//		this.port = port;
+//	}
+//
+//	public void run() {
+//		EventLoopGroup boss = new NioEventLoopGroup();
+//		EventLoopGroup worker = new NioEventLoopGroup();
+//		try {
+//			ServerBootstrap b = new ServerBootstrap();
+//			b.group(boss, worker).channel(NioServerSocketChannel.class)
+//					.option(ChannelOption.SO_BACKLOG, 100)
+//					.childOption(ChannelOption.SO_KEEPALIVE, true)
+//					.handler(new LoggingHandler(LogLevel.INFO))
+//					.childHandler(new ChannelInitializer<SocketChannel>() {
+//						@Override
+//						protected void initChannel(SocketChannel ch)
+//								throws Exception {
+//							ch.pipeline().addLast(
+//									"frameDecoder",
+//									new LengthFieldBasedFrameDecoder(65535, 0,
+//											2, 0, 2));
+//							ch.pipeline()
+//									.addLast(
+//											"msgpack decoder",
+//											new ObjectDecoder(
+//													1024 * 1024,
+//													ClassResolvers
+//															.weakCachingConcurrentResolver(this
+//																	.getClass()
+//																	.getClassLoader())));
+//							ch.pipeline().addLast("frameEncoder",
+//									new LengthFieldPrepender(2));
+//							ch.pipeline().addLast("msgpack encoder",
+//									new ObjectEncoder());
+//							ch.pipeline().addLast(new EchoServerHandler());
+//						}
+//					});
+//
+//			ChannelFuture future = b.bind(port).sync();
+//			future.channel().closeFuture().sync();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		} finally {
+//			boss.shutdownGracefully();
+//			worker.shutdownGracefully();
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		new EchoServer(8000).run();
+//	}
+//
+//}

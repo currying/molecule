8a235431a330b36e886470b6de538f442046b26d
currying <currying@CURRYING-PC>
2015-10-27 19:56:10 +0800

    Signed-off-by: currying <currying@CURRYING-PC>

--- molecule/src/main/java/com/toparchy/molecule/push/webSocket/NettosphereChat.java
+++ molecule/src/main/java/com/toparchy/molecule/push/webSocket/NettosphereChat.java
@@ -1,43 +1,39 @@
-package com.toparchy.molecule.push.webSocket;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
-
-import org.atmosphere.nettosphere.Config;
-import org.atmosphere.nettosphere.Nettosphere;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@WebListener
-public class NettosphereChat implements ServletContextListener {
-
-	private static final Logger logger = LoggerFactory.getLogger(Nettosphere.class);
-
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
-		Config.Builder b = new Config.Builder();
-		b.resource(Chat.class)
-				// For *-distrubution
-				.resource("./webapps")
-				// For mvn exec:java
-				.resource("./src/main/resources")
-				// For running inside an IDE
-				.resource("./nettosphere-samples/chat/src/main/resources").port(8080).host("0.0.0.0").build();
-		Nettosphere s = new Nettosphere.Builder().config(b.build()).build();
-		s.start();
-
-		logger.info("NettoSphere Chat Server started on port {}", 8080);
-		logger.info("Type quit to stop the server");
-	}
-
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-
-	}
-
-}
+//package com.toparchy.molecule.push.webSocket;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.ServletContextEvent;
+//import javax.servlet.ServletContextListener;
+//import javax.servlet.annotation.WebListener;
+//
+//import org.atmosphere.cpr.AtmosphereResource;
+//import org.atmosphere.nettosphere.Config;
+//import org.atmosphere.nettosphere.Handler;
+//import org.atmosphere.nettosphere.Nettosphere;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@WebListener
+//public class NettosphereChat implements ServletContextListener {
+//
+//	@Override
+//	public void contextInitialized(ServletContextEvent sce) {
+//		Nettosphere server = new Nettosphere.Builder()
+//				.config(new Config.Builder().host("127.0.0.1").port(8088).resource(new Handler() {
+//					public void handle(AtmosphereResource r) {
+//						try {
+//							r.getResponse().write("Hello World").write(" from Nettosphere").flushBuffer();
+//						} catch (IOException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//				}).build()).build();
+//		server.start();
+//	}
+//
+//	@Override
+//	public void contextDestroyed(ServletContextEvent sce) {
+//
+//	}
+//
+//}

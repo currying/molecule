d1c8bd98e6d5cb9d13dc65399f301b557f1d074e
currying <currying@CURRYING-PC>
2015-10-29 20:45:16 +0800

    Signed-off-by: currying <currying@CURRYING-PC>

--- molecule/src/main/java/com/toparchy/molecule/permission/LoginController.java
+++ molecule/src/main/java/com/toparchy/molecule/permission/LoginController.java
@@ -1,5 +1,8 @@
 package com.toparchy.molecule.permission;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -19,19 +22,40 @@ public class LoginController {
 	@Inject
 	private FacesContext facesContext;
 
+	// @Inject
+	// private OnlineAccountChecker onlineAccountChecker;
+
 	public String login() {
 		AuthenticationResult result = identity.login();
 		if (AuthenticationResult.FAILED.equals(result)) {
-			facesContext.addMessage(null, new FacesMessage(
-					"Authentication was unsuccessful.  Please check your username and password "
+			facesContext.addMessage(null,
+					new FacesMessage("Authentication was unsuccessful.  Please check your username and password "
 							+ "before trying again."));
 			return null;
 		}
+		// saveOnlineAccount(identity);
 		return "success";
 	}
 
 	public String logout() {
+		// onlineAccountChecker.getOnlineAccount().remove(identity.getAccount().getId());
 		identity.logout();
 		return "success";
 	}
+
+	// public void saveOnlineAccount(Identity identity) {
+	// Iterator<String> it =
+	// onlineAccountChecker.getOnlineAccount().keySet().iterator();
+	// while (it.hasNext()) {
+	// String key = it.next();
+	// // if (key.equals(onlineAccount.get(key).getAccount().getId()))
+	// // onlineAccount.get(key).logout();
+	// System.out.println(key + " 111 " +
+	// onlineAccountChecker.getOnlineAccount().get(key).getAccount());
+	// }
+	// System.out.println(identity.getAccount().getId() + " 222 " +
+	// identity.getAccount());
+	// onlineAccountChecker.getOnlineAccount().put(identity.getAccount().getId(),
+	// identity);
+	// }
 }
---
+++ molecule/src/main/java/com/toparchy/molecule/permission/OnlineAccountChecker.java
@@ -0,0 +1,22 @@
+// package com.toparchy.molecule.permission;
+//
+// import java.util.HashMap;
+// import java.util.Map;
+//
+// import javax.enterprise.context.ApplicationScoped;
+//
+// import org.picketlink.Identity;
+//
+// @ApplicationScoped
+// public class OnlineAccountChecker {
+// private Map<String, Identity> onlineAccount = new HashMap<String,
+// Identity>();
+//
+// public Map<String, Identity> getOnlineAccount() {
+// return onlineAccount;
+// }
+//
+// public void setOnlineAccount(Map<String, Identity> onlineAccount) {
+// this.onlineAccount = onlineAccount;
+// }
+// }
